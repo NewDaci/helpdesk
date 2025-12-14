3f7e53b42281661ecde4f87d6a6ab02cc1c1f72b
NewDaci <ai.ranjt@gmail.com>
2025-12-15 00:11:01 +0530

    update readme.md

--- helpdesk/helpdesk/urls.py
+++ helpdesk/helpdesk/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.http import JsonResponse
 
 urlpatterns = [
+    # welcoming page
+    path('', lambda request: JsonResponse({"message": "Welcome to Helpdesk API. Visit /api/docs/ for API documentation."})),
     path('admin/', admin.site.urls),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
